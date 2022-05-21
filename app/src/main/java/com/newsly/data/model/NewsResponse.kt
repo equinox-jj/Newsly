bf4a6ebd2aa710bbf86f07767bcfdc14a8e00251
equinox-jj <joshua.jeremia@rocketmail.com>
2022-05-21 16:10:49 +0700

    Final.

--- app/src/main/java/com/newsly/data/model/NewsResponse.kt
+++ app/src/main/java/com/newsly/data/model/NewsResponse.kt
@@ -7,30 +7,17 @@ import kotlinx.parcelize.RawValue
 
 
 data class NewsResponse(
-
-    @field:SerializedName("totalResults") val totalResults: Int? = null,
     @field:SerializedName("articles") val articles: List<ArticlesItem>,
-    @field:SerializedName("status") val status: String? = null
-
 )
 
 @Parcelize
 data class ArticlesItem(
 
-    @field:SerializedName("publishedAt") val publishedAt: String? = null,
     @field:SerializedName("author") val author: String? = null,
     @field:SerializedName("urlToImage") val urlToImage: String? = null,
     @field:SerializedName("description") val description: String? = null,
-    @field:SerializedName("source") val source: @RawValue Source,
     @field:SerializedName("title") val title: String? = null,
     @field:SerializedName("url") val url: String,
     @field:SerializedName("content") val content: String? = null
 
 ) : Parcelable
-
-data class Source(
-
-    @field:SerializedName("name") val name: String? = null,
-    @field:SerializedName("id") val id: String? = null
-
-)
